3789c6e07492156119dcdf6be135ef3273ad18ff
Allan Hyde <allan.hyde@tektools.com.au>
2020-12-02 17:56:27 +0800

    Slim Init

---
+++ config/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+use Slim\App;
+
+return function (App $app) {
+    $app->get('/', \App\Action\HomeAction::class)->setName('home');
+    $app->post('/users', \App\Action\UserCreateAction::class);
+};
+
+
---
+++ src/Action/HomeAction.php
@@ -0,0 +1,20 @@
+namespace App\Action;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+final class HomeAction
+{
+    public function __invoke(
+        ServerRequestInterface $request, 
+        ResponseInterface $response
+    ): ResponseInterface {
+       // $response->getBody()->write('Hello, World!');
+       // return $response;
+
+	$response->getBody()->write(json_encode(['success' => true]));
+
+        return $response->withHeader('Content-Type', 'application/json');
+
+    }
+}
